05bd35cd0b58b0ef3963d7b7f009fd9e075e962c
TejaswiniSruthi <82491774+TejaswiniSruthi@users.noreply.github.com>
2022-10-19 18:17:19 +0530

    Create OddsFollowedByEvens.py

---
+++ OddsFollowedByEvens.py
@@ -0,0 +1,12 @@
+n=int(input())
+arr=list(map(int,input().split()))
+ar2=[]
+ar1=[]
+for i in arr:
+    if(i%2!=0):
+        ar2.append(i)
+    else:
+        ar1.append(i)
+arr=(ar2+ar1)
+for i in arr:
+    print(i,end=' ')
