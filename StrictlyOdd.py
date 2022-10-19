6b14edf52c938ca3fd6bda93338719303ed40a61
TejaswiniSruthi <82491774+TejaswiniSruthi@users.noreply.github.com>
2022-10-19 19:57:19 +0530

    Create StrictlyOdd.py

---
+++ StrictlyOdd.py
@@ -0,0 +1,10 @@
+def joy(arr,n):
+    for i,ele in enumerate(arr):
+        if(ele%2!=0):
+            if(i%2==0):
+                return False
+    return True
+        
+n=int(input())
+arr=list(map(int,input().split()))
+print(joy(arr,n))
