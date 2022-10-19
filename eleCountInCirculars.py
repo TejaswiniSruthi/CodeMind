4f03e4581f09e921aa01de74fd0641a7fd576a3e
TejaswiniSruthi <82491774+TejaswiniSruthi@users.noreply.github.com>
2022-10-19 18:38:18 +0530

    Create eleCountInCirculars.py

---
+++ eleCountInCirculars.py
@@ -0,0 +1,8 @@
+n=int(input())
+arr=list(map(int,input().split()))
+arr=[arr[n-1]]+arr+[arr[0]]
+c=0
+for i in range(1,n+1):
+    if((arr[i-1]%2==0 and arr[i+1]%2!=0) or (arr[i-1]%2!=0 and arr[i+1]%2==0)):
+        c+=1
+print(c)
