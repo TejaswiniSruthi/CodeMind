254818d230ba4c237c1c4f1ec11aebe61fe0e17b
TejaswiniSruthi <82491774+TejaswiniSruthi@users.noreply.github.com>
2022-10-20 22:15:15 +0530

    Create Prime<=K.py

---
+++ Prime<=K.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+def isPrime(n):
+    if(n<=1):
+        return False
+    for i in range(2,int(sqrt(n))+1):
+        if(n%i==0):
+            return False
+    return True
+
+n=int(input())
+arr=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in arr:
+    if isPrime(i) and i<=k:
+        c+=1
+print(c)
